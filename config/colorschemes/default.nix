30d1bf66d4f776c2d0ec692f9a5b776e7612bb4c
seism <dcalejandro667@proton.me>
2024-11-20 14:30:49 -0300

    change cscheme

--- config/colorschemes/default.nix
+++ config/colorschemes/default.nix
@@ -15,7 +15,6 @@
   };
   config = lib.mkIf config.colorschemes.enable {
     base16.enable = lib.mkDefault false;
-    catppuccin.enable = lib.mkDefault true;
     rose-pine.enable = lib.mkDefault false;
   };
 }
